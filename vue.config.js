7d985d2585a8acc1e6f9ac0babac8e9c541cc1f6
wdjana <dw.widjanarko@gmail.com>
2023-03-12 06:10:38 +0700

    initial setup

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+const { defineConfig } = require('@vue/cli-service');
+
+
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+      port: 3500,
+        allowedHosts: 'all'
+    },
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('ptgmp-admin', path.resolve(__dirname, 'src/ptgmp-admin'));
+
+        config.resolve.alias
+            .set('common', path.resolve(__dirname, 'src/common'));
+
+        // console.log({ config });
+        config.entryPoints.delete('app');
+        config.entry('app').add('./src/ptgmp-admin/app.js').end().end();
+    },
 })
